99db7b57278aaa069bc48a6613a1d5acd08da589
Sergey Kozlov <>
2018-06-25 23:56:45 +0100

    change server API url

--- src/services/ResourceService.js
+++ src/services/ResourceService.js
@@ -2,16 +2,16 @@ import * as axios from 'axios'
 
 export class ResourceService {
   static saveResource (resource) {
-    return axios.post('http://127.0.0.1:8081/retranslator/v1/resources/', resource)
+    return axios.post('/retranslator/v1/resources/', resource)
   }
 
   static async getListResources () {
-    const response = await axios.get('http://127.0.0.1:8081/retranslator/v1/resources/')
+    const response = await axios.get('/retranslator/v1/resources/')
     return response.data
   }
 
   static async getResource (resourceName) {
-    const response = await axios.get('http://127.0.0.1:8081/retranslator/v1/resources/' + resourceName)
+    const response = await axios.get('/retranslator/v1/resources/' + resourceName)
     return response.data
   }
 }
